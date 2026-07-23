8f5ef8db3f6cf9b7d03fb839e1c173050eb54fba
Manish-webx <kushwahamanish956541@gmail.com>
2026-07-23 16:23:47 +0530

    'ci/cd'

--- about-us.php
+++ about-us.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 
- 
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About - Prayag Hospital</title>
+    <title>Abouts - Prayag Hospital</title>
 
     <?php include 'header-links.php'; ?>
 
