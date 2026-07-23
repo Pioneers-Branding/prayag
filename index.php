672494d890238a6ad3fef2d1abe4d44f0685da21
Manish-webx <kushwahamanish956541@gmail.com>
2026-07-23 16:29:39 +0530

    'checking'

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Prayag Hospital - Healthcare Excellencee</title>
+    <title>Prayag Hospital - Healthcare Excellence</title>
 
     <?php include 'header-links.php'; ?>
 
