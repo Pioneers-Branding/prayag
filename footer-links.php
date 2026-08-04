e9584d1b732c7d240a0ee2c7c61f8fd12ac1abe4
Manish-webx <kushwahamanish956541@gmail.com>
2026-08-04 17:11:21 +0530

    popup add

--- footer-links.php
+++ footer-links.php
@@ -11,3 +11,18 @@
 <!-- Voxforce AI Assistant -->
 <script src="https://staging.voxforce.ai/embed/v1/embed.js" data-persona-id="06c23265-9b23-4e9c-900e-fe4b3db4608c"
    data-organization-id="ab84bd00-0e99-4f08-83c8-254330d032b6" async></script>
+
+
+<script>
+   if (!document.querySelector('#ftamvic9mj')) {
+      var newDiv = document.createElement('div');
+      newDiv.setAttribute('key', 'Popup Congrats Dark');
+      newDiv.setAttribute('class', 'ft');
+      newDiv.setAttribute('id', 'ftamvic9mj');
+      var newScript = document.createElement('script');
+      newScript.setAttribute('src', 'https://wdg.fouita.com/widgets/0x4f35ce.js');
+      newDiv.appendChild(newScript);
+
+      document.body.appendChild(newDiv);
+   }
+</script>
